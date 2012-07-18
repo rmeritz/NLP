bec66347675ff1e0489fa124309cabc8331fbe31
Rebecca Meritz <rjmeritz@gmail.com>
2012-07-18 21:11:50 +0200

    Start Soundex implementation

--- soundex.rb
+++ soundex.rb
@@ -1,4 +1,27 @@
 #!/usr/bin/env ruby
 # -*- coding: utf-8 -*-
 
+## The Soundex Algorithim - A phonetic algorithim
+
+### Tries to encode homophones with the same representation.
+### Produces a four-character strings composed.
+### A single letter followed by three numbers.
+
 require 'rubygems'
+
+name = ARGV.first
+
+first_letter=name[0,1].capitalize
+
+rest=name[1..-1].delete "ehiouwy"
+
+rest.gsub!(/[bfpv]/, '1')
+rest.gsub!(/[cgjkqsxz]/, '2')
+rest.gsub!(/[dt]/, '3')
+rest.gsub!(/[l]/, '4')
+rest.gsub!(/[mn]/, '5')
+rest.gsub!(/[r]/, '6')
+
+puts 
+ 
+
