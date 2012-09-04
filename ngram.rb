c5f91069ec88e6fc1b430ee889acb7fb3083bc9a
Rebecca Meritz <rjmeritz@gmail.com>
2012-09-04 21:17:10 +0200

    I can read a file

--- ngram.rb
+++ ngram.rb
@@ -3,56 +3,65 @@
 
 ## N-Grams
 
-class Gram
-  def initalize(gram)
-    @tokens = gram.tokenize
-  end
-  def n
-    @tokens.count
-  end
-  def word
-    @tokens.take(1)
-  end
-  def history
-    @tokens.drop(1)
-  end
-end
+# class Gram
+#   def initalize(gram)
+#     @tokens = gram.tokenize
+#   end
+#   def n
+#     @tokens.count
+#   end
+#   def word
+#     @tokens.take(1)
+#   end
+#   def history
+#     @tokens.drop(1)
+#   end
+# end
 
 class Corpus
-  def initilize(source)
-    string = File.read(source)
-    @tokens = string.tokenize
+  def initialize(file)
+    @file = file
+    @tokens = File.read(@file)
   end
-  def count(gram)
+  def tokens
+    @tokens
+  end
+  # def count(gram)
     
-  end
+  # end
 
-  private 
-  def is_match(gram, tokens, bool)
-    matching = gram.word
-    rest = gram.history
-    match_index = tokens.find_index(matching)
-    case match_index
-      when nil then bool
-      else 
-    boolean, rest_of_tokens
-  end
+  # private 
+  # def is_match(gram, tokens, bool)
+  #   matching = gram.word
+  #   rest = gram.history
+  #   match_index = tokens.find_index(matching)
+  #   case match_index
+  #     when nil then bool
+  #     else 
+  #   boolean, rest_of_tokens
+  # end
 end
 
 
-class Probablity
-  def initalize(source, gram)
-    @corpus = Corpus.new(source)
-    @gram = Gram.new(gram)
-  end
-  def simple_unsmoothed
-    @corpus.count(@gram)/@corpus.count(@gram.word)
- end
-end
+# class Probablity
+#   def initalize(source, gram)
+#     @corpus = Corpus.new(source)
+#     @gram = Gram.new(gram)
+#   end
+#   def simple_unsmoothed
+#     @corpus.count(@gram)/@corpus.count(@gram.word)
+#  end
+# end
 
 source = ARGV[0]
-gram = ARGV[1]
+# gram = ARGV[1]
 
-probablity = Probablity.new(source, gram)
+# probablity = Probablity.new(source, gram)
 
-puts probablity.simple_unsmoothed
+# puts probablity.simple_unsmoothed
+
+puts source
+
+corpus = Corpus.new(source)
+
+puts corpus.tokens
