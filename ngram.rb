7016cccfb4f2fad51a73bf125c055c48834a713b
Rebecca Meritz <rjmeritz@gmail.com>
2012-09-04 23:11:50 +0200

    Start fix probablity

--- ngram.rb
+++ ngram.rb
@@ -4,72 +4,56 @@
 ## N-Grams
 
 class Gram
-
-  attr_reader :tokens
-  
+  attr_reader :gram
   def initialize(gram)
-    @tokens = gram
+    @gram = gram
   end
   def n
-    @tokens.length
+    @gram.length
   end
   def word
-    @tokens.take(1)
+    @gram.first
   end
   def history
-    @tokens.drop(1)
+    @gram.drop(1)
   end
 end
 
 class Corpus
-
-  attr_reader :file, :tokens
-  
+  attr_reader :file
   def initialize(file)
     @file = file
     @file_contents = File.read(@file)
-    @tokens = @file_contents.split(/\s=+/)
+    @tokens = @file_contents.split(/\s+/)
   end
-  # def count(gram)
-    
-  # end
-
-  # def is_match(gram, tokens, bool)
-  #   matching = gram.word
-  #   rest = gram.history
-  #   match_index = tokens.find_index(matching)
-  #   case match_index
-  #     when nil then bool
-  #     else 
-  #   boolean, rest_of_tokens
-  # end
+  def count_word(gram)
+    @tokens.count(gram.word)
+  end
+  def count_gram(gram)
+    gram.gram.length #FIXME
+  end
 end
 
-
 class Probablity
-  
   attr_reader :corpus, :gram
-  
   def initialize(source, gram)
     @corpus = Corpus.new(source)
     @gram = Gram.new(gram)
   end
-#   def simple_unsmoothed
-#     @corpus.count(@gram)/@corpus.count(@gram.word)
-#  end
+ def simple_unsmoothed
+   gram_count = @corpus.count_gram(@gram)
+   puts "The gram count is #{gram_count}."
+   word_count = @corpus.count_word(@gram)
+   puts "The word count is #{word_count}."
+   p = gram_count/word_count
+   puts "The simple unsmoothed probablity for the #{@gram.n}-gram " +
+     "#{@gram.gram} in the corpus from #{@corpus.file} is #{p}." +
+     "That means there is a #{p*100}% chance that the word " +
+     "\"#{@gram.word}\" would follow the words \"#{@gram.history}\"."
+  end
 end
 
 input_file = ARGV[0]
 input_gram = ARGV[1..-1]
 
-probablity = Probablity.new(input_file, input_gram)
-
-# puts probablity.simple_unsmoothed
-
-puts probablity
-puts probablity.gram
-puts probablity.gram.tokens
-puts probablity.gram.tokens.class
-puts probablity.gram.n
-puts probablity.gram.word.class
-puts probablity.gram.history.class
+Probablity.new(input_file, input_gram).simple_unsmoothed
