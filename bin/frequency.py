96f41ce20f760245f2c1223b2fdd8ef6922fc304
Rebecca Meritz <rebecca@meritz.com>
2015-11-27 15:51:21 +0100

    Create word cloud of popular words
    - Find words in properties that sold for 60% more than their asking price.
      Remove words that also showed up frequencly in properties that sold for 60%
      less than their asking price. Show that.

--- bin/frequency.py
+++ bin/frequency.py
@@ -12,6 +12,7 @@ from pytagcloud import create_tag_image, make_tags
 connection = connect(host="dw.hemnet.se", user="analyst", dbname="gobbler_production")
 
 cursor = connection.cursor()
+stop = stopwords.words('swedish')
 
 cursor.execute("""select pld.description
                   from property_listing_descriptions as pld,
@@ -19,8 +20,8 @@ cursor.execute("""select pld.description
                  where pl.id = pld.property_listing_id and
                  ((pl.sold_price - pl.asking_price) / pl.asking_price::float) > 0.6;""")
 
+
 counted = defaultdict(int)
-stop = stopwords.words('swedish')
 
 for record in cursor:
     if record[0]:
@@ -30,12 +31,27 @@ for record in cursor:
             if not word in stop and len(word) > 1:
                 counted[word] += 1
 
+cursor.execute("""select pld.description
+                  from property_listing_descriptions as pld,
+                         property_listings as pl
+                 where pl.id = pld.property_listing_id and
+                 ((pl.asking_price - pl.sold_price) / pl.asking_price::float) > 0.6;""")
+
+for record in cursor:
+    if record[0]:
+        tokens = word_tokenize(record[0].decode("utf-8"), language="swedish")
+        for token in tokens:
+            word = token.lower()
+            if not word in stop and len(word) > 1:
+                counted[word] -= 1
+
+
 print "Done counting words. Generating image..."
 
 sorted_tag_counts = sorted(counted.iteritems(), key=itemgetter(1), reverse=True)
 tag_counts = [(word, count) for word, count in sorted_tag_counts if count > 4]
 
 tags = make_tags(tag_counts, maxsize=120, minsize=5)
-create_tag_image(tags, 'popular_wordz.png', size=(900, 600), fontname='Reenie Beanie', background=(0,0,0))
+create_tag_image(tags, 'diff_wordz.png', size=(900, 600), fontname='Reenie Beanie', background=(0,0,0))
 
 print "Image generated!"
